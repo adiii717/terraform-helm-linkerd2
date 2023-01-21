abcc14bf496d507d65112c7025eee8fbea2da1c6
Adil M <adilm717@gmail.com>
2023-01-21 07:46:06 +0500

    fix(certs): remove algorithm as its readonly

--- anchor-cert.tf
+++ anchor-cert.tf
@@ -6,7 +6,6 @@ resource "tls_private_key" "trustanchor_key" {
 }
 
 resource "tls_self_signed_cert" "trustanchor_cert" {
-  key_algorithm         = tls_private_key.trustanchor_key.algorithm
   private_key_pem       = tls_private_key.trustanchor_key.private_key_pem
   validity_period_hours = 87600
   is_ca_certificate     = true
@@ -29,7 +28,6 @@ resource "tls_private_key" "issuer_key" {
 }
 
 resource "tls_cert_request" "issuer_req" {
-  key_algorithm   = tls_private_key.issuer_key.algorithm
   private_key_pem = tls_private_key.issuer_key.private_key_pem
 
   subject {
@@ -39,7 +37,6 @@ resource "tls_cert_request" "issuer_req" {
 
 resource "tls_locally_signed_cert" "issuer_cert" {
   cert_request_pem      = tls_cert_request.issuer_req.cert_request_pem
-  ca_key_algorithm      = tls_private_key.trustanchor_key.algorithm
   ca_private_key_pem    = tls_private_key.trustanchor_key.private_key_pem
   ca_cert_pem           = tls_self_signed_cert.trustanchor_cert.cert_pem
   validity_period_hours = 8760
